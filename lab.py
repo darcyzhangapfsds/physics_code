e5d4ad40d73a80c862616650bc0149c8e689ead6
darcyzhangapfsds <heatfs189@gmail.com>
2020-09-20 22:08:54 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,9 +1,10 @@
 #import tensorflow as tf
 #from tensorflow import keras
-#import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 #import pandas as import pd
 import math 
+from statistics import mean
 
 
 def calc_average(num):
@@ -40,17 +41,47 @@ points = [
     [10.0, (6.67*(10**-7))]
 ]
 
-#x, y = [], []
-#for i in range(len(points)):
-#    x.append(points[i][0])
-#    y.append(points[i][1])
+points2 = [
+    [2.0, 1/(1.67*(10**-5))],
+    [4.0, 1/(4.17*(10**-6))],
+    [6.0, 1/(1.85*(10**-6))],
+    [8.0, 1/(1.04*(10**-6))],
+    [10.0, 1/(6.67*(10**-7))]
+]
+
+points3 =[
+    [2.0**2, 1/(1.67*(10**-5))],
+    [4.0**2, 1/(4.17*(10**-6))],
+    [6.0**2, 1/(1.85*(10**-6))],
+    [8.0**2, 1/(1.04*(10**-6))],
+    [10.0**2, 1/(6.67*(10**-7))]
+]
 
 
 
 x, y = [], []
 for i in range(len(points)):
-    x.append(points[i][0])
-    y.append(points[i][1])
+    x.append(points2[i][0])
+    y.append(points2[i][1])
 
-plt.scatter(x, y)
+a, b = [], []
+for i in range(len(points)):
+    a.append(points3[i][0])
+    b.append(points3[i][1])
+
+xs = np.array(a, dtype=np.float64)
+ys = np.array(b, dtype=np.float64)
+
+def lobf(x,y):
+    m = (((mean(x)*mean(y))-mean(x*y))/((mean(x)*mean(x))-mean(x*x)))
+    b = mean(y)-(m*mean(x))
+    return m, b
+
+plt.style.use('seaborn-whitegrid')
+plt.scatter(x, y, c = 'red')
+plt.scatter(a, b, c = 'green')
+#plt.plot(a, b, regression)*****
+plt.title("test plot now xd")
+plt.xlabel("distance in m")
+plt.ylabel("fg except its not rlly a force xd")
 plt.show()
