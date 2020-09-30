0e41de4156134601f68baec4bb334b6c0a5c74ed
darcyzhangapfsds <heatfs189@gmail.com>
2020-09-30 13:37:16 -0600

    created calc.py

--- calc.py
+++ calc.py
@@ -1,9 +1,21 @@
+'''
 import numpy
 import math
+'''
 
-def p(m, v):
-    m = float(m)
-    v = float(v)
-    return m*v
+def p(mass, velocity):
+    mass = float(mass)
+    velocity = float(velocity)
+    return mass*velocity
 
-print(p(6.65*10**-27, 5*10**6))
+def p2(Force, time):
+    Force = float(Force)
+    time = float(time)
+    return Force*time
+
+def a(mass, Force):
+    mass = float(mass)
+    Force = float(Force)
+    return Force/mass
+
+print((a(500, 30))*0.6)
